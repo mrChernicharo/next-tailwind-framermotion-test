5c04d4e5311357af475726422fc218980b66c69b
Felipe Chernicharo <string7dev@gmail.com>
2021-02-17 15:30:30 -0300

    componente Image do next

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+
 const Header: React.FC = () => {
   return (
-    <header className="flex justify-center py-2">
-      <img src="/img/logo.svg" alt="Logo" />
+    <header className="flex justify-center py-2 md:py-4">
+      <Image src="/img/logo.svg" alt="Logo" width={155} height={33} />
     </header>
   );
 };
