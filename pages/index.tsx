2532599cd2743143cd20f308a0c0513511ccef3c
Felipe Chernicharo <string7dev@gmail.com>
2021-02-17 15:01:07 -0300

    primeiro componente

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import React from "react";
+import Layout from '../components/Layout'
+
 export default function Home() {
-  return <h1>Felipe Chernicharo</h1>
+  return (
+  <Layout>
+    <h1>Felipe Chernicharo</h1>
+  </Layout>
+  );
 }
