19abf66b2df7382871e01adef01fdef6b9e355bd
Felipe Chernicharo <string7dev@gmail.com>
2021-02-17 15:26:49 -0300

    Header ing e estilo base

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,8 @@
 const Header: React.FC = () => {
-  return <div></div>;
+  return (
+    <header className="flex justify-center py-2">
+      <img src="/img/logo.svg" alt="Logo" />
+    </header>
+  );
 };
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import Layout from "../components/Layout";
+import React from 'react';
+import Layout from '../components/Layout';
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Felipe Chernicharo</h1>
+      <div></div>
     </Layout>
   );
 }
