44b3bd663f8896d92647f0f0708f582811d61c7f
Felipe Chernicharo <string7dev@gmail.com>
2021-02-17 16:50:05 -0300

    animação entre rotas com AnimateSharedLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/tailwind.css';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
+import { AnimateSharedLayout } from 'framer-motion';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimateSharedLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimateSharedLayout>
   );
 }
 
