4a0322da7515ff6963d6b0546bd7650394cfec05
Felipe Chernicharo <string7dev@gmail.com>
2021-02-17 15:21:34 -0300

    Layout => tailwind classes at work

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,26 @@
-const Layout: React.FC = ({children}) => {
-	return <div>{children}</div>
-}
+import Header from './Header';
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div className="max-w-5xl mx-auto px-4 sm:px-6 md:px-8">
+      <Header />
+      {children}
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
+
+// 1rem == 16px
+
+// px-1 == padding right-left 4px | 0.25rem
+// px-4 == padding right-left 16px | 1rem
+
+// mx-auto => margin right-left auto (centraliza a div)
+
+// max-w- 3xl == max-width 768px | 48rem
+// max-w- 4xl == max-width 896px | 56rem
+// max-w- 5xl == max-width 1024px | 64rem
+
+//breakpoints
+// sm:
+// md:
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-  <Layout>
-    <h1>Felipe Chernicharo</h1>
-  </Layout>
+    <Layout>
+      <h1>Felipe Chernicharo</h1>
+    </Layout>
   );
 }
